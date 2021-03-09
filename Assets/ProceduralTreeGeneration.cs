634cdf999971b2ce880959d695cae38394b76fb0
RoadMan32 <79418681+RoadMan32@users.noreply.github.com>
2021-03-09 22:15:24 +0100

    Wuw

---
+++ Assets/ProceduralTreeGeneration.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralTreeGeneration : MonoBehaviour
+{
+    public GameObject Tree;
+    public int TreeAmount;
+    public LayerMask Ground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generate();
+    }
+    void Generate()
+    {
+        for (int i = 0; i < TreeAmount; i++)
+        {
+            transform.position = new Vector3(Random.Range(-29f, 24f), transform.position.y, 0);
+            RaycastHit2D RayHitPos;
+
+            RayHitPos = Physics2D.Raycast(transform.position, Vector2.down * 1, 30f, Ground);
+            Instantiate(Tree, RayHitPos.point, Quaternion.identity);
+        }
+    }
+
+}
